51831e2ad796170954f8481e1c36f1acccb339f4
Francisco Juan <juansoussev@gmail.com>
2024-02-15 18:25:53 -0300

    [aula02.15] css component

---
+++ css-component/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyCssComponent from './components/MyCssComponent'
+
+function App() {
+
+  return (
+    <>
+      <h1>CSS GLOBAL</h1>
+      <MyCssComponent />
+    </>
+  )
+}
+
+export default App
---
+++ css-component/src/components/MyCssComponent.jsx
@@ -0,0 +1,11 @@
+import './MyCssComponent.css';
+
+const MyCssComponent = () => {
+  return (
+    <div>
+      <h1>CSS COMPONENT</h1>
+    </div>
+  )
+}
+
+export default MyCssComponent
